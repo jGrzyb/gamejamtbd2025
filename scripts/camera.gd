af54167bc1c8fae57e5e85badab1fcf795146420
jGrzyb <grzybjaku@gmail.com>
2025-04-05 17:44:24 +0200

    test

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -3,4 +3,4 @@ extends Camera2D
 @onready var player: Player = %Player
 
 func _physics_process(delta: float) -> void:
-	position = position.lerp(player.position, 0.05)
+	position = position.lerp(player.position, 0.05) 
