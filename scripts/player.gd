7b215316595148cfffef621a415a00f3c8c3879a
jGrzyb <grzybjaku@gmail.com>
2025-04-05 20:29:51 +0200

    animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,9 @@ var game_manager: GameManager
 
 @onready var timer_dash: Timer = $Timer_dash
 @onready var area_2d: Area2D = $Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer_attack: Timer = $Timer_attack
+@onready var timer_die: Timer = $Timer_die
 
 @export var speed := 100
 @export var dash_speed := 400
@@ -13,6 +16,7 @@ var game_manager: GameManager
 
 var last_direction := Vector2(0, 1)
 var can_dash := true
+var is_basic_anim := true
 
 
 func _ready() -> void:
@@ -21,6 +25,8 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
+	if is_basic_anim:
+		choose_animation(direction)
 	velocity = velocity.lerp(direction * speed, 0.1)
 	if direction != Vector2.ZERO:
 		last_direction = direction
@@ -33,6 +39,9 @@ func _input(event: InputEvent) -> void:
 		velocity = last_direction * dash_speed
 		timer_dash.start()
 	elif event.is_action_pressed("attack"):
+		is_basic_anim = false
+		animated_sprite_2d.play("attack")
+		timer_attack.start()
 		var enemies = area_2d.get_overlapping_bodies().filter(func(x): return x is Enemy)
 		for e in enemies:
 			(e as Enemy).hit(self)
@@ -40,3 +49,31 @@ func _input(event: InputEvent) -> void:
 
 func _on_timer_dash_timeout() -> void:
 	can_dash = true
+
+
+func choose_animation(direction: Vector2):
+	if direction.x > 0:
+		animated_sprite_2d.play("right")
+	elif direction.x < 0:
+		animated_sprite_2d.play("left")
+	elif direction.y < 0:
+		animated_sprite_2d.play("up")
+	elif direction.y > 0:
+		animated_sprite_2d.play("down")
+
+
+func die() -> void:
+	if timer_die.is_stopped():
+		timer_attack.stop()
+		is_basic_anim = false
+		animated_sprite_2d.play("die")
+		timer_die.start()
+
+
+func _on_timer_die_timeout() -> void:
+	game_manager.game_over()
+
+
+func _on_timer_attack_timeout() -> void:
+	is_basic_anim = true
+	animated_sprite_2d.play("down")
